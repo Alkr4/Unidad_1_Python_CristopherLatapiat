c53f79d0087462b70ca55c58117c661687f18174
Cristopher <cris.al.lat.bar@gmail.com>
2025-08-26 18:31:38 -0400

    Agregando Modelo Dispositivo

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -15,3 +15,13 @@ class Zona (models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class Dispositivo (models.Model):
+    nombre = models.CharField(max_length=50)
+    consumo_maximo = models.IntegerField()
+    estado = models.BooleanField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    zona = models.ForeignKey(Zona, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
