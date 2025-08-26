cda58375199991406cfaba5e8d0335e8766090de
Cristopher <cris.al.lat.bar@gmail.com>
2025-08-26 18:02:22 -0400

    Creacion Models categoria

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
