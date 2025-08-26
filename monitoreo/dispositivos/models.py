623dd0bc6641af5f42a0a023ff7961919b9f0454
Cristopher <cris.al.lat.bar@gmail.com>
2025-08-26 18:23:42 -0400

    Agregando Modelo Zona

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -8,3 +8,10 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class Zona (models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
