5cca239a5b156d2db5f819c305744878ec1f9f99
0ppilif <filippomoreno.128@gmail.com>
2025-08-28 01:39:59 -0400

    aaaaaaaaaaaaaaaaaaaa

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Medicion(models.Model):
+    valor_consumo = models.CharField(max_length=50)
+    fecha_hora = models.DateTimeField(blank=True, null=True)
+    dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE, related_name='mediciones')
+
+    def __str__(self):
+        return f"{self.dispositivo.nombre} - {self.valor_consumo} - {self.fecha_hora}"
+
+
+class Alerta(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(blank=True, null=True)
+    fecha_hora = models.DateTimeField(blank=True, null=True)
+    dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE, related_name='alertas')
+
+    def __str__(self):
+        return self.nombre
