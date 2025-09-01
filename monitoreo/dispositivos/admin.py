be1ea97d7012947b36f8873dbcb6060adf788c66
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-09-01 16:56:52 -0400

    Algunas cosas de django admin

--- monitoreo/dispositivos/admin.py
+++ monitoreo/dispositivos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Categoria, Zona, Dispositivo
+admin.site.register([Categoria, Zona])
+@admin.register(Dispositivo)
+class DispositivoAdmin(Dispositivo):
+    list_display('nombre','consumo_maximo','categoria','estado','zona')
